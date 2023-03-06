98d4948ce2724953b28f63738e88dda9bc7d5602
Vincent Granville <vincentg@mltechniques.com>
2023-03-05 20:54:41 -0800

    GAN to synthetize tabular data (diabetes dataset)

--- GAN_diabetes.py
+++ GAN_diabetes.py
@@ -25,6 +25,7 @@ print (data.shape)
 print (data.tail())
 print (data.columns)
 
+
 seed = 103     # to make results replicable
 np.random.seed(seed)     # for numpy
 random.set_seed(seed)    # for tensorflow/keras
@@ -34,6 +35,7 @@ adam = Adam(learning_rate=0.001) # also try 0.01
 latent_dim = 10
 n_inputs   = 9   # number of features
 n_outputs  = 9   # number of features
+
 
 #--- STEP 1: Base Accuracy for Real Dataset
 
@@ -94,14 +96,37 @@ def define_gan(generator, discriminator):
     model.compile(loss='binary_crossentropy', optimizer=adam)  
     return model
 
-def train(g_model, d_model, gan_model, latent_dim, n_epochs=10000, n_batch=128, n_eval=200): 
+def gan_distance(data, model, latent_dim, nobs_synth): 
+
+    # generate nobs_synth synthetic rows as X, and return it as data_fake
+    # also return correlation distance between data_fake and real data
+
+    latent_points = generate_latent_points(latent_dim, nobs_synth)  
+    X = model.predict(latent_points, verbose=0)  
+    data_fake = pd.DataFrame(data=X,  columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome'])
+ 
+    # convert Outcome field to binary 0/1
+    outcome_mean = data_fake.Outcome.mean()
+    data_fake['Outcome'] = data_fake['Outcome'] > outcome_mean
+    data_fake["Outcome"] = data_fake["Outcome"].astype(int)
+
+    # compute correlation distance
+    R_data      = np.corrcoef(data.T) # T for transpose
+    R_data_fake = np.corrcoef(data_fake.T)
+    g_dist = np.average(abs(R_data-R_data_fake))
+    return(g_dist, data_fake) 
+
+def train(g_model, d_model, gan_model, latent_dim, mode, n_epochs=10000, n_batch=128, n_eval=50):   
     
     # determine half the size of one batch, for updating the  discriminator
     half_batch = int(n_batch / 2)
     d_history = [] 
     g_history = [] 
+    g_dist_history = []
+    if mode == 'Enhanced':
+        g_dist_min = 999999999.0  
 
-    for epoch in range(n_epochs+1): 
+    for epoch in range(0,n_epochs+1): 
                  
         # update discriminator
         x_real, y_real = generate_real_samples(half_batch)  # sample from real data
@@ -117,15 +142,35 @@ def train(g_model, d_model, gan_model, latent_dim, n_epochs=10000, n_batch=128, 
         d_history.append(d_loss)
         g_history.append(g_loss_fake)
 
+        if mode == 'Enhanced': 
+            (g_dist, data_fake) = gan_distance(data, g_model, latent_dim, nobs_synth=400)
+            if g_dist < g_dist_min and epoch > int(0.75*n_epochs): 
+               g_dist_min = g_dist
+               best_data_fake = data_fake
+               best_epoch = epoch
+        else: 
+            g_dist = -1.0
+        g_dist_history.append(g_dist)
+                
         if epoch % n_eval == 0: # evaluate the model every n_eval epochs
-            print('>%d, d1=%.3f, d2=%.3f d=%.3f g=%.3f' % (epoch, d_loss_real, d_loss_fake, d_loss,  g_loss_fake))       
+            print('>%d, d1=%.3f, d2=%.3f d=%.3f g=%.3f g_dist=%.3f' % (epoch, d_loss_real, d_loss_fake, d_loss,  g_loss_fake, g_dist))       
             plt.subplot(1, 1, 1)
             plt.plot(d_history, label='d')
             plt.plot(g_history, label='gen')
             # plt.show() # un-comment to see the plots
             plt.close()
-            
-    return(g_model)
+       
+    OUT=open("history.txt","w")
+    for k in range(len(d_history)):
+        OUT.write("%6.4f\t%6.4f\t%6.4f\n" %(d_history[k],g_history[k],g_dist_history[k]))
+    OUT.close()
+    
+    if mode == 'Standard':
+        # best synth data is assumed to be the one produced at last epoch
+        best_epoch = epoch
+        (g_dist_min, best_data_fake) = gan_distance(data, g_model, latent_dim, nobs_synth=400)
+       
+    return(g_model, best_data_fake, g_dist_min, best_epoch) 
 
 #--- main part for building & training model
 
@@ -134,33 +179,18 @@ discriminator.summary()
 generator = define_generator(latent_dim, n_outputs)
 generator.summary()
 gan_model = define_gan(generator, discriminator)
-model = train(generator, discriminator, gan_model, latent_dim)
 
+mode = 'Enhanced'  # options: 'Standard' or 'Enhanced'
+model, data_fake, g_dist, best_epoch = train(generator, discriminator, gan_model, latent_dim, mode)
+data_fake.to_csv('diabetes_synthetic.csv') 
+    
 
-#--- STEP 3: Evaluate the Quality of Generated Fake Data With Model
+#--- STEP 3: Classify synthetic data based on Outcome field
 
-# Get nobs_synth random vectors in latent space of dimension latent_dim 
-nobs_synth = 400
-latent_points = generate_latent_points(latent_dim, nobs_synth)  
-
-# produces nobs_synth synthetized data points in feature space
-X = model.predict(latent_points, verbose=0)              
-data_fake = pd.DataFrame(data=X,  columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome'])
-data_fake.head()
-
-# Outcome is 0 or 1. Need map the value of the generated fake data to 0 or 1.
-outcome_mean = data_fake.Outcome.mean()
-data_fake['Outcome'] = data_fake['Outcome'] > outcome_mean
-data_fake["Outcome"] = data_fake["Outcome"].astype(int)
-data_fake.to_csv('diabetes_synthetic.csv')
-
-# do the same feature engineering in the fake data (label is Outcome column)
 features = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
 label = ['Outcome']
 X_fake_created = data_fake[features]
 y_fake_created = data_fake[label]
-
-# classification of synthetic data for comparison with classification of real data done before GAN
 X_fake_train, X_fake_test, y_fake_train, y_fake_test = train_test_split(X_fake_created, y_fake_created, test_size=0.30, random_state=42)
 clf_fake = RandomForestClassifier(n_estimators=100)
 clf_fake.fit(X_fake_train,y_fake_train)
@@ -169,7 +199,7 @@ print("Accuracy of fake data model: %5.3f" % (metrics.accuracy_score(y_fake_test
 print("Classification report of fake data model:\n",metrics.classification_report(y_fake_test, y_fake_pred))
 
 
-#--- STEP 4: Evaluate the Quality of Generated Fake Data With Table_evaluator
+#--- STEP 4: Evaluate the Quality of Generated Fake Data With g_dist and Table_evaluator
 
 from table_evaluator import load_data, TableEvaluator
 
@@ -177,8 +207,5 @@ table_evaluator = TableEvaluator(data, data_fake)
 table_evaluator.evaluate(target_col='Outcome')
 # table_evaluator.visual_evaluation() 
 
-R_data      = np.corrcoef(data.T) # T for transpose
-R_data_fake = np.corrcoef(data_fake.T)
-#print(R_data)
-#print(R_data_fake)
-print("Avg correlation distance: %5.3f" % (np.average(abs(R_data-R_data_fake))))
+print("Avg correlation distance: %5.3f" % (g_dist))
+print("Based on epoch number: %5d" % (best_epoch))
